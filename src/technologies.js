729c25d1fbd140833941738a4261a820dc493a2f
Artur <arturel101010@mail.ru>
2019-09-10 11:31:58 +0300

    added project

---
+++ src/technologies.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
+}
+
+export default  Technologies;
