1f21594fe934f61a852d4c5d2402d67c7ed2d8bb
Artur <arturel101010@mail.ru>
2019-09-12 13:40:41 +0300

    Освоил map. Сделал рефакторинг кода!

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -19,15 +19,14 @@ const Message =  (props) => {
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         { id: 0, name: 'Вася' },
         { id: 1, name: 'Ира' },
         { id: 2, name: 'Лена' },
         { id: 3, name: 'Ваня' },
         { id: 4, name: 'Маша' },
     ];
-
-    let messagesData = [
+    let messages = [
         { id: 0, massage: 'Hello, my name is Johny' },
         { id: 1, massage: 'How are you?' },
         { id: 2, massage: 'I am comming from Germany' },
@@ -35,21 +34,18 @@ const Dialogs = (props) => {
         { id: 4, massage: 'Yo' },
     ];
 
+    let dialogsItem = dialogs.map(el => (<DialogItem name={el.name} id={el.id} />));
+    let messagesItems = messages.map(message => (<Message id= {message.id} text={message.massage}/>));
+
+
+
     return(
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-
+                {dialogsItem}
             </div>
             <div className={classes.messages}>
-                <Message id= {messagesData[0].id} text={messagesData[0].massage}/>
-                <Message id= {messagesData[1].id} text={messagesData[1].massage}/>
-                <Message id= {messagesData[2].id} text={messagesData[2].massage}/>
-                <Message id= {messagesData[3].id} text={messagesData[3].massage}/>
-                <Message id= {messagesData[4].id} text={messagesData[4].massage}/>
-
+                {messagesItems}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -4,13 +4,15 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
 
-    let postData = [
+    let posts = [
         { id: 0, massage: 'Hello it is my first post', countLikes: 15 },
         { id: 1, massage: 'oh what to write here', countLikes: 20 },
         { id: 2, massage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, nostrum?', countLikes: 22 },
         { id: 3, massage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', countLikes: 3 },
         { id: 4, massage: 'Lorem ipsum dolor sit amet', countLikes: 100},
     ];
+
+    let postsItems = posts.map(post => <Post message = {post.massage} countLikes={post.countLikes}/> );
 
     return (
             <div className={classes.postsBlock}>
@@ -24,11 +26,7 @@ const MyPosts = () => {
                     </div>
                 </div>
                 <div>
-                    <Post message = {postData[0].massage} countLikes={postData[0].countLikes}/>
-                    <Post message = {postData[1].massage} countLikes={postData[1].countLikes}/>
-                    <Post message = {postData[2].massage} countLikes={postData[2].countLikes}/>
-                    <Post message = {postData[3].massage} countLikes={postData[3].countLikes}/>
-                    <Post message = {postData[4].massage} countLikes={postData[4].countLikes}/>
+                    { postsItems }
                 </div>
             </div>
     )
