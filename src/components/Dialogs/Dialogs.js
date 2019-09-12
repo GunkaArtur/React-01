7f4952173df17c94ea2a1e5f61d3d4fa6c6b7dec
Artur <arturel101010@mail.ru>
2019-09-12 13:57:36 +0300

    Сделал отдельные компоненды для Диалога и Сообщения

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,21 +1,8 @@
 import  React from 'react';
 import classes from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
-const DialogItem = (props) => {
-    let path = '/dialogs/'+props.id;
-    return (
-        <div className={classes.dialog}>
-            <NavLink to = {path}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message =  (props) => {
-    return (
-        <div className={classes.message}>{props.text}</div>
-    )
-}
 
 const Dialogs = (props) => {
 
