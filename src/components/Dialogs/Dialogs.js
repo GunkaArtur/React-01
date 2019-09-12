31aa54463ee3f872168641dcd0f5163f9de31e51
Artur <arturel101010@mail.ru>
2019-09-12 13:13:11 +0300

    Вынес данные в обьекты

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -18,19 +18,37 @@ const Message =  (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        { id: 0, name: 'Вася' },
+        { id: 1, name: 'Ира' },
+        { id: 2, name: 'Лена' },
+        { id: 3, name: 'Ваня' },
+        { id: 4, name: 'Маша' },
+    ];
+
+    let messagesData = [
+        { id: 0, massage: 'Hello, my name is Johny' },
+        { id: 1, massage: 'How are you?' },
+        { id: 2, massage: 'I am comming from Germany' },
+        { id: 3, massage: 'I hate BMW' },
+        { id: 4, massage: 'Yo' },
+    ];
+
     return(
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name='Вася' id='1' />
-                <DialogItem name='Ира' id='2' />
-                <DialogItem name='Лена' id='3' />
-                <DialogItem name='Ваня' id='4' />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+
             </div>
             <div className={classes.messages}>
-                <Message text='Hello, my name is Johny'/>
-                <Message text='How are you?'/>
-                <Message text='I am comming from Germany'/>
-                <Message text='I hate BMW'/>
+                <Message id= {messagesData[0].id} text={messagesData[0].massage}/>
+                <Message id= {messagesData[1].id} text={messagesData[1].massage}/>
+                <Message id= {messagesData[2].id} text={messagesData[2].massage}/>
+                <Message id= {messagesData[3].id} text={messagesData[3].massage}/>
+                <Message id= {messagesData[4].id} text={messagesData[4].massage}/>
 
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,10 +3,33 @@ import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        { id: 0, massage: 'Hello it is my first post', countLikes: 15 },
+        { id: 1, massage: 'oh what to write here', countLikes: 20 },
+        { id: 2, massage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, nostrum?', countLikes: 22 },
+        { id: 3, massage: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', countLikes: 3 },
+        { id: 4, massage: 'Lorem ipsum dolor sit amet', countLikes: 100},
+    ];
+
     return (
-            <div>
-                <Post message = 'Hello it is my first post' countLikes='15'/>
-                <Post message = 'oh what to write here ' countLikes='20'/>
+            <div className={classes.postsBlock}>
+                <h3>My Posts:</h3>
+                <div className={classes.addPostBlock}>
+                    <div>
+                        <textarea></textarea>
+                    </div>
+                    <div>
+                        <button>Add post</button>
+                    </div>
+                </div>
+                <div>
+                    <Post message = {postData[0].massage} countLikes={postData[0].countLikes}/>
+                    <Post message = {postData[1].massage} countLikes={postData[1].countLikes}/>
+                    <Post message = {postData[2].massage} countLikes={postData[2].countLikes}/>
+                    <Post message = {postData[3].massage} countLikes={postData[3].countLikes}/>
+                    <Post message = {postData[4].massage} countLikes={postData[4].countLikes}/>
+                </div>
             </div>
     )
 }
